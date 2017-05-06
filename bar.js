65c03d9820ff4514f5122e387a82261cdb92b425
kleina3 <kleina3@carleton.edu>
2017-05-06 17:56:49 -0500

    working barebones of bar chart

---
+++ bar.js
@@ -0,0 +1,125 @@
+/* Code by Adam Klein and Veronica Child */
+
+w = 600;
+h = 2000;
+margin = 30;
+buffer = 60;
+axisOffset = 50;
+padding = 5;
+barWidth = 10;
+
+vals = ['FIPStxt', 'State', 'Area_Name', 'PCTPOVALL_2015', 'MEDHHINC_2015'];
+
+d3.csv('povertyData.csv', function(csvData) {
+    data = csvData;
+    
+    svg = d3.select('#barSVG').append('svg:svg')
+        .attr('width', w)
+        .attr('height', h);
+    
+    //scales poverty % data
+    //note range is from right to left
+    povScale = d3.scale.linear()
+        .domain([d3.min(data, function(d) { return parseFloat(d[vals[3]]); })-1,
+                 d3.max(data, function(d) { return parseFloat(d[vals[3]]); })+1]) 
+        .range([(w-margin)/2 - (buffer/2), margin]);
+    
+    //scales median income data
+    incScale = d3.scale.linear()
+        .domain([d3.min(data, function(d) { return parseFloat(d[vals[4]]); })-1,
+                 d3.max(data, function(d) { return parseFloat(d[vals[4]]); })+1])
+        .range([((w-margin)/2 + (buffer/2)), (w-margin)]);
+    
+    // Build axes!
+    povAxis = d3.svg.axis()
+        .scale(povScale) // scale to x range
+        .orient('top')
+        .ticks(5);
+
+    // Create a group element for the axis called
+    povAxisG = svg.append('g')
+        .attr('class', 'axis')
+        .attr('transform', 'translate(0,' + axisOffset + ')')
+        .call(povAxis); // calls axis function
+    
+    // Build axes!
+    incAxis = d3.svg.axis()
+        .scale(incScale) // scale to x range
+        .orient('top')
+        .ticks(5);
+
+    // Create a group element for the axis called
+    incAxisG = svg.append('g')
+        .attr('class', 'axis')
+        .attr('transform', 'translate(0,' + axisOffset + ')')
+        .call(incAxis); // calls axis function
+    
+    drawBars();
+});
+
+//draws the bars
+function drawBars() {
+    //for interaction, might come back and make both
+    //sides the same variable "bar"
+    var povBar = svg.selectAll('.pov')
+        .data(data);
+
+    counter = axisOffset;
+    
+    povBar.enter()
+        .append('svg:rect')
+        .filter(function(d){
+            if ( (d[vals[0]]%1000) == 0) {return d;}
+        })
+        .attr('class', '.pov')
+        //.attr('class', '.pov')
+        .attr('height', barWidth)
+        .attr('y', function() {
+            counter += padding + barWidth;
+            return counter;
+        })
+        .attr('x', function(d) {
+            //console.log(d);
+            return povScale(d[vals[3]]);
+        })
+        .attr('width', function(d) {
+            results = (((w-margin)/2) - (buffer/2)) - povScale(d[vals[3]]);
+            return results;
+        })
+        .style('fill', 'green');
+        
+    var incBar = svg.selectAll('.inc')
+        .data(data);
+    
+    counter = axisOffset;
+    
+    incBar.enter()
+        .append('svg:rect')
+        .filter(function(d){
+            if ( (d[vals[0]]%1000) == 0) {return d;}
+        })
+        .attr('class', '.inc')
+        .attr('height', barWidth)
+        .attr('y', function() {
+            counter += padding + barWidth;
+            return counter;
+        })
+        .attr('x', function(d) {
+            console.log(d['MEDHHINC_2015']);
+            return (w-margin)/2 + (buffer/2);
+        })
+        .attr('width', function(d) {
+            results = w - incScale(d[vals[4]]);
+            console.log(d);
+            console.log(d[vals[4]]);
+            console.log(results);
+            return results;
+        /*
+            if (results < 0) {
+                console.log(d);
+                return 0;}
+        */
+        })
+        .style('fill', 'red');
+        
+}
