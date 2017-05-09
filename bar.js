68d86ad109d886a6b11cfefb42adc5869b06376a
kleina3 <kleina3@carleton.edu>
2017-05-08 21:41:26 -0500

    fixed margin shenanigans, added labels

--- bar.js
+++ bar.js
@@ -1,12 +1,16 @@
 /* Code by Adam Klein and Veronica Child */
 
-w = 600;
+w = 800;
 h = 2000;
-margin = 30;
-buffer = 60;
-axisOffset = 50;
+//outer margin of the svg
+margin = 60;
+//space between the two bar graphs
+buffer = 100;
+//y-coord where the axes begin
+axisOffset = margin + 50;
+//space between bars
 padding = 5;
-barWidth = 10;
+barWidth = 15;
 
 vals = ['FIPStxt', 'State', 'Area_Name', 'PCTPOVALL_2015', 'MEDHHINC_2015'];
 
@@ -20,15 +24,17 @@ d3.csv('povertyData.csv', function(csvData) {
     //scales poverty % data
     //note range is from right to left
     povScale = d3.scale.linear()
+        /*
         .domain([d3.min(data, function(d) { return parseFloat(d[vals[3]]); })-1,
-                 d3.max(data, function(d) { return parseFloat(d[vals[3]]); })+1]) 
-        .range([(w-margin)/2 - (buffer/2), margin]);
+                 d3.max(data, function(d) { return parseFloat(d[vals[3]]); })+1])
+        */
+        .domain([0, d3.max(data, function(d) { return parseFloat(d[vals[3]]); })+1])
+        .range([w/2 - (buffer/2), margin]);
     
     //scales median income data
     incScale = d3.scale.linear()
-        .domain([d3.min(data, function(d) { return parseFloat(d[vals[4]]); })-1,
-                 d3.max(data, function(d) { return parseFloat(d[vals[4]]); })+1])
-        .range([((w-margin)/2 + (buffer/2)), (w-margin)]);
+        .domain([0, d3.max(data, function(d) { return parseFloat(d[vals[4]]); })+1])
+        .range([(w/2 + (buffer/2)), w-margin]);
     
     // Build axes!
     povAxis = d3.svg.axis()
@@ -53,6 +59,46 @@ d3.csv('povertyData.csv', function(csvData) {
         .attr('class', 'axis')
         .attr('transform', 'translate(0,' + axisOffset + ')')
         .call(incAxis); // calls axis function
+    
+    //title
+    svg.append("text")
+        .attr('x', w/2)
+        .attr('y', margin/2)
+        .attr('text-anchor', 'middle')
+        .text("Poverty to Median Income by State")
+        .attr("font-size", "15px")
+        .attr("font-weight", "bold")
+        .attr("fill", "black");
+    
+    //state label
+    svg.append("text")
+        .attr('x', w/2)
+        .attr('y', axisOffset-25)
+        .attr('text-anchor', 'middle')
+        .text("State")
+        .attr("font-size", "13px")
+        .attr("font-weight", "bold")
+        .attr("fill", "black");
+    
+    //poverty label
+    svg.append("text")
+        .attr('x', (margin + ((w - (margin*2)) - buffer)/4))
+        .attr('y', axisOffset-25)
+        .attr('text-anchor', 'middle')
+        .text("% in Poverty")
+        .attr("font-size", "13px")
+        .attr("font-weight", "bold")
+        .attr("fill", "black");
+    
+    //income label
+    svg.append("text")
+        .attr('x', w - (margin + ((w - (margin*2)) - buffer)/4))
+        .attr('y', axisOffset-25)
+        .attr('text-anchor', 'middle')
+        .text("Median Income ($)")
+        .attr("font-size", "13px")
+        .attr("font-weight", "bold")
+        .attr("fill", "black");
     
     drawBars();
 });
@@ -83,7 +129,7 @@ function drawBars() {
             return povScale(d[vals[3]]);
         })
         .attr('width', function(d) {
-            results = (((w-margin)/2) - (buffer/2)) - povScale(d[vals[3]]);
+            results = ((w/2) - (buffer/2)) - povScale(d[vals[3]]);
             return results;
         })
         .style('fill', 'green');
@@ -106,10 +152,10 @@ function drawBars() {
         })
         .attr('x', function(d) {
             console.log(d['MEDHHINC_2015']);
-            return (w-margin)/2 + (buffer/2);
+            return w/2 + (buffer/2);
         })
         .attr('width', function(d) {
-            results = w - incScale(d[vals[4]]);
+            results = (w - (margin)) - incScale(d[vals[4]]);
             console.log(d);
             console.log(d[vals[4]]);
             console.log(results);
@@ -121,5 +167,30 @@ function drawBars() {
         */
         })
         .style('fill', 'red');
+    
+    var barText = svg.selectAll('.names')
+        .data(data);
         
+    counter = axisOffset + barWidth;
+    
+    barText.enter()
+        .append('svg:text')
+        .filter(function(d){
+            if ( (d[vals[0]]%1000) == 0) {return d;}
+        })
+        .attr('class', '.names')
+        .attr('text-anchor', 'middle')
+        .attr('x', (w/2))
+        .attr('y', function() {
+            counter += padding + barWidth;
+            return counter;
+        })
+        .text(function(d){
+            return d[vals[2]];
+        });
 }
+
+//sorts the data by given index
+//function sort(valIndex) {
+    
+//}
